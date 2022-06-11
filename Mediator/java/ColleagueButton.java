2b2aed8f8bd411b298f9029d2a50e09a62b1c813
yuyayuya <s1t5y3y1@gmail.com>
2022-06-11 22:11:35 +0900

    Add Mediator (java)

---
+++ Mediator/java/ColleagueButton.java
@@ -0,0 +1,19 @@
+import java.awt.Button;
+
+public class ColleagueButton extends Button implements Colleague {
+    private Mediator mediator;
+
+    public ColleagueButton(String caption) {
+        super(caption);
+    }
+
+    @Override
+    public void setMediator(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    @Override
+    public void setColleagueEnabled(boolean enabled) {
+        setEnabled(enabled);
+    }
+}
